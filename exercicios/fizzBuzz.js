f9f9f74a7fca46e1a0e764ae29a4e30bdc019b10
Filipe Vila Nova <filipevn@gmail.com>
2019-06-30 11:42:28 -0300

    add exercicios de JS

---
+++ exercicios/fizzBuzz.js
@@ -0,0 +1,21 @@
+//Divisível por 3 => Fizz
+//Divisível por 5 => Buzz
+//Divisível por 3 e 5 => FizzBuzz
+//Não Divisível por 3 e/ou 5 => Não FizzBuzz
+//Não é um número => 'Não é um número" 
+
+const resultado = (t);
+console.log(typeof resultado);
+/*
+function fizzBuzz (entrada){
+    if (typeof entrada !== 'number')
+        return 'Não é um numero';
+    if (entrada % 3 === 0 && entrada % 5 === 0)
+        return 'FizzBuzz';
+    if (entrada % 3 === 0)
+        return 'Fizz';
+    if (entrada % 5 === 0)
+        return 'Buzz';
+        return 'Não é um Número';
+}
+*/
---
+++ exercicios/maximoentredois.js
@@ -0,0 +1,15 @@
+//Escreva uma função que use 2 números e retorne o maior entre eles
+
+let a = 0
+let b = 20
+let x = max()
+
+console.log(x)
+function max(){
+    return a > b ? a: b; // forma de fazer 1 (operador ternario)
+
+   /* if (a > b) // forma de fazer 2
+    return a;
+    else (b > a)
+    return b;*/
+}
